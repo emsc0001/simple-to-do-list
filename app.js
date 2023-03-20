671c579bb39144722cb4fa3d8aaf8a89f85feddd
emsc0001 <124031667+emsc0001@users.noreply.github.com>
2023-03-20 15:13:51 +0100

    To Do List

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 "use strict";
+window.addEventListener("load", initApp);
+
+const newToDo = document.querySelector("#input-new-to-do");
+const list = document.querySelector("#to-do-list");
+
+function initApp() {
+  document.querySelector("#btn-add-item").addEventListener("click", addToDo);
+}
+
+//   const listText = document.createElement("li");
+//   const span = document.createElement("span");
+//   const button = document.createElement("button");
+
+//   span.textContent = newToDo.value;
+//   button.textContent = "Delete";
+
+//   listText.appendChild(span);
+//   listText.appendChild(button);
+//   list.appendChild(listText);
+
+//   button.addEventListener("click", removeToDo);
+// }
+
+// // function removeToDo() {
+// //   this.parentNode.remove();
+// / }/
+
+function addToDo() {
+  const myHTML = /*html*/ `
+<li> 
+  <span>${newToDo.value}</span>
+    <button>Delete</button>
+ </li>
+`;
+  list.insertAdjacentHTML("beforeend", myHTML);
+
+  list.querySelector("li:last-child button").addEventListener("click", removeToDo);
+}
+
+function removeToDo () {
+    this.parentNode.remove();
+}
